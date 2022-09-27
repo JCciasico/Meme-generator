b8fe3d8154901d35afceb238097d27dee5291cf0
JCCiasico <carlociasico01@gmail.com>
2022-09-27 09:10:54 +0800

    chore: added meme body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/header/Header'
+import Meme from './components/meme-body/Meme'
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <Meme/>
     </div>
   )
 }
